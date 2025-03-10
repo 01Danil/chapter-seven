4e0e21d440c78d29586ba362db0cb7aa971a77bb
Danil Kucheryavko <danil.kucheryavko@gmail.com>
2025-03-10 12:13:12 +0000

    Initial commit and example useEffect. Page 151

---
+++ src/components/checkbox.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+
+function Checkbox() {
+  const [checked, setChecked] = useState(false);
+
+  /*
+  useEffect(() => {
+    alert(`cheked: ${checked.toString()}`);
+  });
+	*/
+  /*
+  useEffect(() => {
+    console.log(checked ? "Yes, chacked" : "No, not checked");
+  });
+	*/
+  useEffect(() => {
+    localStorage.setItem("checkbox-value", checked);
+  });
+
+  return (
+    <>
+      <input
+        type="checkbox"
+        value={checked}
+        onChange={() => setChecked((checked) => !checked)}
+      />
+      {checked ? "checked" : "not checked"}
+    </>
+  );
+}
