216fc93de39e7b0b73b6f8a03d29c66b7198376a
Danil Kucheryavko <danil.kucheryavko@gmail.com>
2025-03-11 13:13:20 +0000

    Add finished example useCallback. Page 162

--- src/components/useCallback.js
+++ src/components/useCallback.js
@@ -10,6 +10,7 @@ useEffect(() => {
 }, [fn]);
 */
 
+/*
 const fn = useCallback(() => {
   console.log("hello");
   console.log("word");
@@ -19,3 +20,28 @@ useEffect(() => {
   console.log("fresh render");
   fn();
 }, [fn]);
+*/
+
+const useJazzyNews = () => {
+  const [_posts, setPosts] = useState([]);
+  const addPost = (post) => setPosts((allPosts) => [post, ...allPosts]);
+
+  const posts = useMemo(() => _posts, [_posts]);
+
+  useEffect(() => {
+    newPostChime.play();
+  }, [posts]);
+
+  useEffect(() => {
+    newsFeed.subscribe(addPost);
+    return () => newsFeed.unsubscribe(addPost);
+  }, []);
+
+  useEffect(() => {
+    welcomeChime.play();
+    return () => {
+      goodbyeChime.play();
+    };
+  }, []);
+  return posts;
+};
