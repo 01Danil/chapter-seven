cdeefcbf920223d721beecad5d0c7124d6e50a27
Danil Kucheryavko <danil.kucheryavko@gmail.com>
2025-03-13 10:05:30 +0000

    Example useReducer. Page 169

--- src/components/checkbox.js
+++ src/components/checkbox.js
@@ -1,35 +1,29 @@
-import React, { useState } from "react";
-
+/*
 function Checkbox() {
-  const [checked, setChecked] = useState(false);
+  const [cheked, setChecked] = useState(false);
 
-  /*
-  useEffect(() => {
-    alert(`cheked: ${checked.toString()}`);
-  });
-	*/
-  /*
-  useEffect(() => {
-    console.log(checked ? "Yes, chacked" : "No, not checked");
-  });
-	*/
-  /*
-  useEffect(() => {
-    localStorage.setItem("checkbox-value", checked);
-  });
-	*/
-  useEffect(() => {
-    txtInputRef.current.focus();
-  });
+  function toggle() {
+    setChecked((cheked) => !cheked);
+  }
 
   return (
     <>
-      <input
-        type="checkbox"
-        value={checked}
-        onChange={() => setChecked((checked) => !checked)}
-      />
-      {checked ? "checked" : "not checked"}
+      <input type="checkbox" value={cheked} onChange={toggle} />
+      {cheked ? "cheked" : "not cheked"}
+    </>
+  );
+}
+*/
+
+const { useReducer } = require("react");
+
+function Checkbox() {
+  const [checked, toggle] = useReducer((checked) => !checked, false);
+
+  return (
+    <>
+      <input type="checkbox" value={cheked} onChange={toggle} />
+      {checked ? "checked" : "not cheked"}
     </>
   );
 }
