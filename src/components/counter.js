8c78acdd35ad79c225d5be30f4656ac558510226
Danil Kucheryavko <danil.kucheryavko@gmail.com>
2025-03-13 09:40:22 +0000

    Example rules for working with hooks. Page 167

--- src/components/counter.js
+++ src/components/counter.js
@@ -1,3 +1,4 @@
+/*
 function Counter() {
 	const [count, setCount] = useState(0);
 	const [checked, toggle] = useState(false);
@@ -16,3 +17,69 @@ function Counter() {
 
 		return ( ... )
 }
+*/
+
+/*
+const { useState } = require("react");
+
+function Counter() {
+	const [count, setCount] = useState(0);
+
+	if (count > 5) {
+		const [checked, toggle] = useState(false);
+	}
+
+	useEffect(() => {
+	...
+	});
+
+	if(counr > 5) {
+		useEffect(() => {
+			...	
+		});
+	}
+	useEffect(() => {
+		...
+		});
+		return ( ... )
+		}
+}
+*/
+
+/*
+function Couner() {
+	const [count, setCount] = useState(0);
+	const [checked, toggle] =
+	useState(
+	count => (count < 5)
+	? undefined
+	: !c,
+	(count < 5) ? undefined
+	);
+	useEffect(() => {
+		...
+		});
+		useEffect(() => {
+		if (count < 5) return;
+		...
+		});
+		useEffect(() => {
+		...
+		});
+
+		useEffect(() => {
+			const fn = async () => {
+				await SomePromise();
+			}
+			fn()
+		});
+
+		useEffect(() => {
+			(async () => {
+				await SomePromise()
+			})();
+		});
+
+		return ( ... )
+		}
+*/
