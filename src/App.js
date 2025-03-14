3e7b1006c99b0ed6ed08c9b260210dc96392943f
Danil Kucheryavko <danil.kucheryavko@gmail.com>
2025-03-14 11:17:38 +0000

    Add example improving component rendering performance. Page 174

--- src/App.js
+++ src/App.js
@@ -1,7 +1,44 @@
-import React, { useEffect, useLayoutEffect } from "react";
+import React, { useState, memo } from "react";
 
 function App() {
-  useEffect(() => console.log("useEffect"));
-  useLayoutEffect(() => console.timeLog("useLayoutEffect"));
-  return <div>ready</div>;
+  const [cats, setCats] = useState(["Biscuit", "Jungle", "Outlaw"]);
+  return (
+    <>
+      {cats.map((name, i) => (
+        <PureCat key={i} name={name} />
+      ))}
+      <button onClick={() => setCats([...cats, prompt("Name a cat")])}>
+        Add a cat
+      </button>
+    </>
+  );
 }
+
+/*
+const Cat = ({ name }) => {
+  console.log(`rendering ${name}`);
+  return <p>{name}</p>;
+};
+*/
+
+const Cat = memo(({ name, meow = (f) => f }) => {
+  console.log(`rendering ${name}`);
+  <PureCat
+    key={i}
+    name={name}
+    meow={(name) => console.log(`${name} has meowed`)}
+  />;
+  return <p onClick={() => meow(name)}>{name}</p>;
+});
+
+/*
+const PureCat = memo(Cat);
+*/
+
+const PureCat = memo(
+  Cat,
+  (cpevProps, nextProps) => prevProps.name === nextProps.name
+);
+
+const RenderCatOnce = memo(Cat, () => true);
+const AlwaysRenderCat = memo(Cat, () => false);
